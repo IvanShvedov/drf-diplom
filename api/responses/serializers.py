73aaa4c18ff67859e38544fb96d2b10c4b3e3245
IvanShvedov <shvedov540@gmail.com>
2021-04-05 18:21:41 +0300

    fix

--- api/responses/serializers.py
+++ api/responses/serializers.py
@@ -7,8 +7,8 @@ class VacancyResponseSerializer(serializers.ModelSerializer):
     vacancy = serializers.SerializerMethodField('get_vacancy')
     employer_avatar = serializers.SerializerMethodField('get_emp_avatar')
     worker_avatar = serializers.SerializerMethodField('get_wrk_avatar')
-    worker_name = serializers.SerializerMethodField('get_wrk_name')
     employer_name = serializers.SerializerMethodField('get_emp_name')
+    worker_name = serializers.SerializerMethodField('get_wrk_name')
 
     class Meta:
         model = VacancyResponse
@@ -19,14 +19,14 @@ class VacancyResponseSerializer(serializers.ModelSerializer):
 
     def get_wrk_name(self, obj):
         try:
-            worker = Worker.objects.get(id=obj.worker.id)
+            worker = Worker.objects.get(user=obj.worker.id)
             return worker.name
         except Worker.DoesNotExist:
             return ''
 
     def get_emp_name(self, obj):
         try:
-            employer = Employer.objects.get(id=obj.employer.id)
+            employer = Employer.objects.get(user=obj.employer.id)
             return employer.name
         except Employer.DoesNotExist:
             return ''
@@ -40,14 +40,14 @@ class VacancyResponseSerializer(serializers.ModelSerializer):
 
     def get_wrk_avatar(self, obj):
         try:
-            worker = Worker.objects.get(id=obj.worker.id)
+            worker = Worker.objects.get(user=obj.worker.id)
             return worker.photo_url
         except Worker.DoesNotExist:
             return ''
 
     def get_emp_avatar(self, obj):
         try:
-            employer = Employer.objects.get(id=obj.employer.id)
+            employer = Employer.objects.get(user=obj.employer.id)
             return employer.photo_url
         except Employer.DoesNotExist:
             return ''
@@ -69,14 +69,14 @@ class CvResponseSerializer(serializers.ModelSerializer):
 
     def get_wrk_name(self, obj):
         try:
-            worker = Worker.objects.get(id=obj.worker.id)
+            worker = Worker.objects.get(user=obj.worker.id)
             return worker.name
         except Worker.DoesNotExist:
             return ''
 
     def get_emp_name(self, obj):
         try:
-            employer = Employer.objects.get(id=obj.employer.id)
+            employer = Employer.objects.get(user=obj.employer.id)
             return employer.name
         except Employer.DoesNotExist:
             return ''
@@ -90,14 +90,14 @@ class CvResponseSerializer(serializers.ModelSerializer):
 
     def get_wrk_avatar(self, obj):
         try:
-            worker = Worker.objects.get(id=obj.worker.id)
+            worker = Worker.objects.get(user=obj.worker.id)
             return worker.photo_url
         except Worker.DoesNotExist:
             return ''
 
     def get_emp_avatar(self, obj):
         try:
-            employer = Employer.objects.get(id=obj.employer.id)
+            employer = Employer.objects.get(user=obj.employer.id)
             return employer.photo_url
         except Employer.DoesNotExist:
             return ''
