4f7424f182a095cbe2884eeaf4124a7b3329c836
IvanShvedov <shvedov540@gmail.com>
2021-04-15 13:55:54 +0300

    access token test

--- api/api/settings.py
+++ api/api/settings.py
@@ -171,8 +171,7 @@ REST_AUTH_SERIALIZERS = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=15),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
