ac0ff336437543d302b194f66366df8b1fe1644f
IvanShvedov <shvedov540@gmail.com>
2021-04-15 12:27:50 +0300

    order_by id in search

--- api/search/views.py
+++ api/search/views.py
@@ -26,7 +26,7 @@ class CvSearchView(APIView, MyPaginationMixin):
                 pass
             if request.GET:
                 cv = Filter(cv).filt(request)
-                cv = cv.order_by('id')
+                cv = cv.order_by('-pk')
             if payload is not None:
                 context={'user_id': payload.get('user_id')}
             page = self.paginate_queryset(cv)
@@ -54,7 +54,7 @@ class VacancySearchView(APIView, MyPaginationMixin):
                 pass
             if request.GET:
                 vacancy = Filter(vacancy).filt(request)
-                vacancy = vacancy.order_by('id')
+                vacancy = vacancy.order_by('-pk')
             if payload is not None:
                 context={'user_id': payload.get('user_id')}
             page = self.paginate_queryset(vacancy)
