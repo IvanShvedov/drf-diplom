d2bfe0de0a4cd756b0af0113be79a0b05285217b
IvanShvedov <shvedov540@gmail.com>
2021-04-20 23:28:29 +0300

    fix

--- api/responses/views.py
+++ api/responses/views.py
@@ -63,6 +63,7 @@ class CvResponseView(APIView):
             serializer = InCvResponseSerializer(data=request.data)
             if serializer.is_valid(raise_exception=True):
                 serializer.save()
+                print(serializer.data)
                 send_notify_response.delay(serializer.data, 'cv')
             return Response({"id": serializer.data['id']}, status=status.HTTP_201_CREATED)
         except TypeError:
