90d778c47c7a9ea13515b55924f6759bacb22198
IvanShvedov <shvedov540@gmail.com>
2021-04-20 23:20:20 +0300

    fix

--- api/services/mailer.py
+++ api/services/mailer.py
@@ -1,3 +1,4 @@
+from api.main.models import Employer
 from typing import Type
 from django.core.mail import send_mail
 
@@ -9,12 +10,12 @@ class ResponseNotifyService:
     def __init__(self, serializer, response_to: str):
         try:
             if 'vacancy' in response_to:
-                response = VacancyResponse.objects.filter(vacancy=serializer['vacancy'], worker=serializer['worker']).first()
+                response = VacancyResponse.objects.filter(vacancy=serializer['vacancy'], employer=serializer['employer']).first()
                 response_name = response.vacancy.vacancy_name
                 self.subject = 'На вашу вакансию "{0}" откликнулись'.format(response_name)
                 self.to_email = response.employer.email
             elif 'cv' in response_to:
-                response = CvResponse.objects.filter(cv=serializer['cv'], employer=serializer['employer']).first()
+                response = CvResponse.objects.filter(cv=serializer['cv'], worker=serializer['worker']).first()
                 response_name = response.cv.vacancy_name
                 self.subject = 'На ваше резюме "{0}" откликнулись'.format(response_name)
                 self.to_email = response.worker.email
